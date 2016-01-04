3a1446dcdef49a51f00bfbac2a745879e2e825a6
pmlee <pmlee@fxcm.com>
2016-01-04 21:25:39 +0000

    Require that haproxy class be defined

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,6 +8,11 @@ define haproxy::config (
   $custom_fragment = undef,  # A default is required for Puppet 2.7 compatibility. When 2.7 is no longer supported, this parameter default should be removed.
   $merge_options = $haproxy::merge_options,
 ) {
+
+  if ! defined(Class['haproxy']) {
+    fail('Class haproxy must be defined')
+}
+
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
@@ -21,12 +26,12 @@ define haproxy::config (
     warning("${module_name}: The \$merge_options parameter will default to true in the next major release. Please review the documentation regarding the implications.")
   }
 
-  if $config_dir != undef {
+  if $config_dir != $haproxy::params::config_dir {
     file { $config_dir:
       ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
+      owner  => '0',
+      group  => '0',
+      mode   => '0644',
     }
   }
 
